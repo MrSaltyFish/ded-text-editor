e10763df77c28028932ff13e28f511d9204fdf8a
MrSaltyFish <anveshkhode7588@gmail.com>
2025-01-18 05:30:56 +0530

    FINALLY INSTALLED SDL3

---
+++ main.c
@@ -0,0 +1,17 @@
+
+/*
+ *
+ *   Ded (Text Editor) - Copied Project from Tsoding Daily -
+ * https://www.youtube.com/watch?v=2UY_Am-Q-oI&list=PLpM-Dvs8t0VZVshbPeHPculzFFBdQWIFu
+ *
+ */
+
+#include <SDL3/SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+	printf("Hello World\n");
+	getch();
+	return 0;
+}
